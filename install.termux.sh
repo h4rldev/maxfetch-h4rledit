78eb48f1f79638af6fab8602e5d5612e968ca9b7
H4rl <98224660+h4rldev@users.noreply.github.com>
2023-07-20 08:45:14 +0100

    Update install.termux.sh

--- install.termux.sh
+++ install.termux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -s https://raw.githubusercontent.com/jobcmax/maxfetch/main/maxfetch.termux > ~/maxfetch 
+curl -s https://raw.githubusercontent.com/h4rldev/maxfetch-h4rledit/main/maxfetch.termux > ~/maxfetch 
 chmod +x ~/maxfetch
 mv ~/maxfetch /data/data/com.termux/files/usr/bin
 echo "\033[1;34;48mmaxfetch is installed! \033[1;37;0m" 
