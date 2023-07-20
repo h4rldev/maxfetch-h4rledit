a019b8bde1993300b6d0976add9f14d8df7d28cc
H4rl <98224660+h4rldev@users.noreply.github.com>
2023-07-20 08:44:53 +0100

    change install.sh to accomodate my branch specifically (removing aur)

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [ -e /usr/bin/makepkg ]; then
-	printf "using AUR package\n"
-	pushd $(mktemp -d)
-	curl -s 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=maxfetch' -o PKGBUILD
-	makepkg -sci
-else
-	pushd /usr/local/bin
-	sudo curl -s https://raw.githubusercontent.com/jobcmax/maxfetch/main/maxfetch -o maxfetch
-	sudo chmod +x maxfetch
-fi
+#if [ -e /usr/bin/makepkg ]; then
+#	printf "using AUR package\n"
+#	pushd $(mktemp -d)
+#	curl -s 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=maxfetch' -o PKGBUILD
+#	makepkg -sci
+#else
+pushd /usr/local/bin
+sudo curl -s https://raw.githubusercontent.com/h4rldev/maxfetch-h4rledit/main/maxfetch -o maxfetch
+sudo chmod +x maxfetch
+#fi
 popd
 printf "\033[1;34;48mmaxfetch is installed! \033[1;37;0m\n"
 maxfetch
